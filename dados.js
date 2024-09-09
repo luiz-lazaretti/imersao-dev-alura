2629cc4f59eeb6538d819cb62cce00d9b56e937d
Laza <lazarettieb@gmail.com>
2024-09-09 00:02:27 -0300

    finalizacao

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+  {
+    titulo: "Samba",
+    descricao:
+      "Samba, um dos maiores ícones da cultura brasileira, é um gênero musical que nasceu nas comunidades afro-brasileiras do Rio de Janeiro no início do século XX.Do morro para os palcos do mundo!",
+    link: "https://pt.wikipedia.org/wiki/Samba",
+    tags: "samba musica zeca pagodinho",
+  },
+  {
+    titulo: "Funk Carioca",
+    descricao:
+      "O funk carioca, nascido nas favelas do Rio de Janeiro, é um gênero musical que mistura elementos do funk norte-americano com ritmos brasileiros. Conhecido por suas letras que retratam a vida nas comunidades e por suas batidas contagiante, o funk carioca é um dos gêneros musicais mais populares do Brasil.",
+    link: "https://pt.wikipedia.org/wiki/Funk_carioca",
+    tags: "funk favela carioca musica",
+  },
+  {
+    titulo: "Forró",
+    descricao:
+      "O Forró é um ritmo musical popular do Nordeste brasileiro, conhecido por suas letras alegres e danças animadas. É caracterizado pela presença de instrumentos como acordeon, triângulo e zabumba.",
+    link: "https://pt.wikipedia.org/wiki/Forró",
+    tags: "forró forro musica zabumba familia lima",
+  },
+];
